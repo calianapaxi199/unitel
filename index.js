a358715ebcb3670ed6fae0f226b26e7bfc0621db
calianapaxi199 <calianaelisa@outlook.pt>
2022-10-16 14:45:54 +0100

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,17 @@
+alert(
+    'bem vindo'
+);
+var idade = 18;
+var nome= " caliana"
+var n1 =19
+var n2=40
+var n3 =10
+var mult= n1*n2*n3;
+document.write("o resultado da multplicao é",mult,"<br>");
+document.write("<br>")
+if(mul%2==0){ 
+    document.write("o numer");
+}
+else{
+    document.write()
+}
